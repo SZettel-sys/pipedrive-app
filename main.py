0f5da6bd3899177ecf6252aa20e7adb25df05d76
SZettel-sys <simone.zettel@bizforward.de>
2025-09-26 12:41:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -352,4 +352,5 @@ async def overview(request: Request):
 if __name__=="__main__":
     import uvicorn
     port=int(os.environ.get("PORT",8000))
-    uvicorn.run("main:app",host="0.0.0.0",port=port,reload
+    uvicorn.run("main:app",host="0.0.0.0",port=port,reload=False)
+
