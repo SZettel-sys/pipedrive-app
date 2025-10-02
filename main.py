4264a7dd51b96dcc3097d810940bc10d5c97538e
SZettel-sys <simone.zettel@bizforward.de>
2025-10-02 11:35:55 +0200

    Update main.py
    tool-bar fixiert

--- main.py
+++ main.py
@@ -351,6 +351,9 @@ async def overview(request: Request):
           margin-bottom:15px;
           border:1px solid #cce;
           border-radius:8px;
+          position: sticky;
+          top: 0;
+          z-index: 500;
         }
       </style>
     </head>
@@ -539,6 +542,7 @@ if __name__=="__main__":
     import uvicorn
     port=int(os.environ.get("PORT",8000))
     uvicorn.run("main:app",host="0.0.0.0",port=port,reload=False)
+
 
 
 
