6ad7dad27e7046f03689923f3802f2b30c1e2379
SZettel-sys <simone.zettel@bizforward.de>
2025-11-25 09:36:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ async def scan_orgs(threshold: int = 85):
     }
 
 
-# ================== Preview Merge ==================
+# ================== Preview Merge ===================
 @app.post("/preview_merge")
 async def preview_merge(org1_id: int, org2_id: int, keep_id: int):
     headers = get_headers()
@@ -568,6 +568,7 @@ if __name__=="__main__":
     import uvicorn
     port=int(os.environ.get("PORT",8000))
     uvicorn.run("main:app",host="0.0.0.0",port=port,reload=False)
+
 
 
 
